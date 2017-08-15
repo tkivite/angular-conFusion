d251ad8ac2c0692fe90d9ed621eece0c648f9bdd
Titus <tkivite@gmail.com>
2017-08-15 14:36:51 +0300

    angular router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,16 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -21,7 +27,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,    
@@ -29,7 +38,8 @@ import { FooterComponent } from './footer/footer.component';
     FormsModule,
     HttpModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule  
   ],
   providers: [ DishService ],
   bootstrap: [AppComponent]
