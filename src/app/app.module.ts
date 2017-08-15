ae64bbfbad987d6ae839f0bee2afabd6e3f2f33f
Titus <tkivite@gmail.com>
2017-08-16 00:44:41 +0300

    Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -40,6 +41,7 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,    
     BrowserAnimationsModule,  
     FormsModule,
+    ReactiveFormsModule,  
     HttpModule,
     MaterialModule,
     FlexLayoutModule,
