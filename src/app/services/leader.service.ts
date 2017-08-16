45e6a396bf9bc77ef16b458cf1dd10dc53b80e93
Titus <tkivite@gmail.com>
2017-08-16 12:45:57 +0300

    Promises Part two-delays + waiting icon

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -7,11 +7,19 @@ export class LeaderService {
 
   constructor() { }
 getLeaders(): Promise<Leader[]> {
-return Promise.resolve(LEADERS);
+//eturn Promise.resolve(LEADERS);
+      return new Promise(resolve=> {
+// Simulate server latency with 2 second delay
+setTimeout(() => resolve(LEADERS), 2000);
+});
 }
 
     getFeaturedLeader(): Promise<Leader> {
-return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+//return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+        return new Promise(resolve => {        
+        setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]),2000);
+        
+        });
 }
 
 }
