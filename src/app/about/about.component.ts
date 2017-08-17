7404bed084aac80a1ff8be30dbd33515ce633785
Titus <tkivite@gmail.com>
2017-08-17 15:33:17 +0300

    Angular Animations Part2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,22 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
 import { Leader } from '../shared/leader';
 import {LeaderService} from '../services/leader.service';
+
+import { flyInOut,expand } from '../animations/app.animation';
+ 
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()  
+  ]    
 })
 export class AboutComponent implements OnInit {
     
     leaders: Leader[];
+    errMess: string;
 
-  constructor(private leaderService: LeaderService ) { }
+  constructor(private leaderService: LeaderService,@Inject('BaseURL') private BaseURL ) { }
 
   ngOnInit() {
       //this.leaders = this.leaderService.getLeaders();
-      this.leaderService.getLeaders().subscribe(leaders => this.leaders = leaders);
+      this.leaderService.getLeaders().subscribe(leaders => this.leaders = leaders,errmess => this.errMess = <any>errmess);
   }
+      
 
     
     
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit,Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+
+import { flyInOut, visibility,expand } from '../animations/app.animation';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -13,22 +14,20 @@ import { Comment } from '../shared/comment';
 
 
 
+
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    flyInOut(),
+    visibility(),
+    expand()  
   ]    
 })
 export class DishdetailComponent implements OnInit {
